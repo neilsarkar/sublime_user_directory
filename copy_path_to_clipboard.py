b266a368945340511414a2eafa898968a213fc3f
Neil Sarkar <neil@groupme.com>
2012-04-06 19:04:27 -0400

    f1rst

---
+++ copy_path_to_clipboard.py
@@ -0,0 +1,8 @@
+import sublime, sublime_plugin
+import os
+
+class CopyPathToClipboard(sublime_plugin.TextCommand):
+  def run(self, edit):
+    line_number, column = self.view.rowcol(self.view.sel()[0].begin())
+    line_number += 1
+    sublime.set_clipboard(self.view.file_name() + ":" + str(line_number))
---
+++ open_rspec_file.py
@@ -0,0 +1,19 @@
+import sublime
+import sublime_plugin
+import os
+
+class OpenRspecFileCommand(sublime_plugin.WindowCommand):
+  def run(self, option):
+    self.views = []
+    window = self.window
+    current_file_path = self.window.active_view().file_name()
+
+    if current_file_path.find("/spec/") > 0:
+      twin_path = current_file_path.replace("/spec/","/app/").replace("_spec.rb", ".rb")
+    else:
+      twin_path = current_file_path.replace("/app/", "/spec/").replace(".rb", "_spec.rb")
+
+    if os.path.exists(twin_path) :
+      window.open_file(twin_path)
+    else :
+      sublime.status_message("Could not find " + twin_path)
