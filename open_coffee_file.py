ccdf620f9598deb35bebb1a5aafd0445d772b003
Neil Sarkar <neil@groupme.com>
2012-05-14 10:13:02 -0400

    Use ok_cancel_dialog in open_coffee_file.py

--- open_coffee_file.py
+++ open_coffee_file.py
@@ -17,8 +17,8 @@ class OpenCoffeeTwinCommand(sublime_plugin.WindowCommand):
       window.open_file(twin_path)
     else :
       # ok_cancel_dialog is in the API docs but is not implemented yet
-      # if sublime.ok_cancel_dialog("Create file: "+twin_path, "Yeah, fuck it"):
-      open(twin_path,"w").close()
-      window.open_file(twin_path)
-      # else:
-      #   sublime.status_message("Could not find " + twin_path)
+      if sublime.ok_cancel_dialog("Create file: "+twin_path, "Yeah, fuck it"):
+        open(twin_path,"w").close()
+        window.open_file(twin_path)
+      else:
+        sublime.status_message("Could not find " + twin_path)
