d9e9f51198a64eac3ca5208d8d7ec25edcd65b0a
Neil Sarkar <neil@groupme.com>
2012-09-10 11:35:51 -0400

    Run last spec with magic+r

--- rspec.py
+++ rspec.py
@@ -97,7 +97,21 @@ class OpenRspecFileCommand(sublime_plugin.WindowCommand):
 
 
 class RunTests(sublime_plugin.TextCommand):
-  def run(self, edit, single):
+  def run(self, edit, scope):
+    last_run = sublime.load_settings("Rspec.last-run")
+
+    if scope == "last":
+      self.run_spec(last_run.get("root_path"), last_run.get("path"))
+    else:
+      path = self.find_path(scope)
+      root_path = re.sub("\/spec\/.*", "", path)
+      self.run_spec(root_path, path)
+
+      last_run.set("path", path)
+      last_run.set("root_path", root_path)
+      sublime.save_settings("Rspec.last-run")
+
+  def find_path(self, scope):
     path = self.view.file_name()
 
     if path.find("/spec/") < 0:
@@ -107,13 +121,14 @@ class RunTests(sublime_plugin.TextCommand):
       else:
         return sublime.error_message("You're not in a spec, bro.")
 
-    root_path = re.sub("\/spec\/.*", "", path)
-
-    if single:
+    if scope == "line":
       line_number, column = self.view.rowcol(self.view.sel()[0].begin())
       line_number += 1
       path += ":" + str(line_number)
 
+    return path
+
+  def run_spec(self, root_path, path):
     self.run_in_terminal('cd ' + root_path)
     self.run_in_terminal('bundle exec rspec ' + path)
 
